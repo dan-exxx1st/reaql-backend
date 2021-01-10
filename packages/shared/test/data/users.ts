bddb3a086e921d33df60ecf3684e470b8c120393
Daniil Parfenov <dan.exxx1st@gmail.com>
2021-01-10 13:57:52 +0300

    Add tests.
    Init tests in users-service. Add tests which check user ids search.

---
+++ packages/shared/test/data/users.ts
@@ -0,0 +1,41 @@
+const createdAt = new Date(),
+  updatedAt = new Date();
+
+export const UsersMockData = [
+  {
+    id: '1',
+    name: 'Test',
+    surname: 'User1',
+    email: 'testuser1@mail.com',
+    avatar: '',
+    createdAt,
+    updatedAt,
+  },
+  {
+    id: '2',
+    name: 'Test',
+    surname: 'User2',
+    email: 'testuser2@mail.com',
+    avatar: '',
+    createdAt,
+    updatedAt,
+  },
+  {
+    id: '3',
+    name: 'Test',
+    surname: 'User3',
+    email: 'testuser3@mail.com',
+    avatar: '',
+    createdAt,
+    updatedAt,
+  },
+  {
+    id: '4',
+    name: 'Test',
+    surname: 'User4',
+    email: 'testuser4@mail.com',
+    avatar: '',
+    createdAt,
+    updatedAt,
+  },
+];
