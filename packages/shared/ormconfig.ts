f970212e513141127ca7c98383be5147ee5c09b1
Daniil Parfenov <dan.exxx1st@gmail.com>
2021-01-24 15:24:27 +0300

    Add ssl option for postgres db.

--- packages/shared/ormconfig.ts
+++ packages/shared/ormconfig.ts
@@ -4,6 +4,7 @@ import { User, Session, Dialog, DialogProps, Message } from './models';
 const config: ConnectionOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL || 'postgres://postgres:123@localhost:5432/reaql-dev',
+  ssl: Boolean(process.env.DATABASE_URL ? true : false),
   entities: [User, Session, Dialog, DialogProps, Message],
   synchronize: false,
   migrationsRun: false,
